875c51af695b37f498861718b7b974ccb69b3f53
Hubert Wesołowski <hwesol13@gmail.com>
2016-07-20 15:48:35 +0200

    test

--- src/Utils/Config.js
+++
@@ -1,8 +0,0 @@
-const Config = {
-  instanceName: 'yourInstanceNameHere',
-  accountKey: 'yourApiKeyHere',
-  boardClassName: 'tictactoe',
-  playersClassName: 'tictactoeplayers'
-};
-
-export default Config;
--- src/Utils/connection.js
+++
@@ -1,6 +0,0 @@
-import Syncano from 'syncano';
-import Config from './Config';
-
-const Connection = Syncano({accountKey: Config.accountKey});
-
-export default Connection;
